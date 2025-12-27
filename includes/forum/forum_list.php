c0a4516631d25877fbe7705995445680a7eec608
megalamo <previty20@gmail.com>
2025-12-26 23:48:55 -0500

    Update forum_list.php

--- includes/forum/forum_list.php
+++ includes/forum/forum_list.php
@@ -1,104 +1,86 @@
 <?php
-	//number of topics/page
-	$limit = 40;
-	//number of pages to display. number - 1. ex: for 5 value should be 4
-	$page_limit = 6;
+$logger = new logger();
+$logger->log_action($f3->get('checked_user_id'), $_SERVER['REMOTE_ADDR'], 'FORUM_LIST', $f3->get('PARAMS.page'));
+
+//Number of topics/page
+$limit = 40;
+
+//Load required classes
+$misc = new misc();
+$user = new user();
+$resultcount = 0;
+$threadresult = array();
+
+//Query for total number of topics
+$topiccount = $db->exec('SELECT COUNT(id) as count FROM '.$f3->get('forum_topic_table'));
+$numrows = $topiccount[0]["count"];
+
+//Check if user is logged in and set username+id
+if($user->check_log()){
+	$uname = $f3->get('checked_username');
+	$uid = $f3->get('checked_user_id');
+	$canpin = $user->gotpermission('pin_forum_topics');
+	$candelete = $user->gotpermission('delete_forum_topics');
+	$canlock = $user->gotpermission('lock_forum_topics');
+}else{
+    $canpin = false;
+    $canlock = false;
+    $candelete = false;
+}
+
+//Pagination stuff	
+$pages = new Pagination($numrows, $limit);
+$pages->setTemplate('pagination.html');
+$f3->set('pagebrowser', $pages->serve());
+if ($f3->get('PARAMS.page') == ""){
+	$f3->set('PARAMS.page',1);
+}
+
+//Convert page number for db query
+if ($f3->get('PARAMS.page') == 0){
+    $f3->set('PARAMS.page',1);
+}
+$pg_start = $limit*($f3->get('PARAMS.page')-1);
+$pg_curr = $pg_start;
+$pg_end = $pg_start+$limit;
+if ($pg_end > $numrows){
+	$pg_end = $numrows;
+}
+
+//Make sure page is not higher than max rows
+if ($pg_start > $numrows || $pg_start < 0){
+    $f3->reroute('/forum/list/');
+}
+
+//Get forum thread info
+$fthreadinfo = $db->exec('SELECT id, topic, unix_timestamp(last_updated) as last_updated, author, locked, priority FROM '.$f3->get('forum_topic_table').' ORDER BY priority DESC, last_updated DESC LIMIT ?, ?',array(1=>$pg_start,2=>$limit));
+
+//Start loop through threads
+foreach($fthreadinfo as $thread){
+	//Get post count for topic
+	$repliescount = $db->exec('SELECT COUNT(id) as count FROM '.$f3->get('forum_post_table').' WHERE topic_id = ?',array(1=>$thread['id']));
 	
-	header("Cache-Control: store, cache");
-	header("Pragma: cache");
-	$misc = new misc();
-	require "includes/header.php";
-	
-	echo '<form method="post" action="?page=forum&amp;s=search">
-	<input type="text" name="search" value="" style="width: 40%;" />
-	<input type="submit" name="submit" value="Search" />
-	</form>
-	<table class="highlightable" width="100%"><tr><th width="60%">Title</th><th width="10%">Created by</th><th width="10%">Updated</th><th width="5%">Replies</th>';
-	if(isset($_GET['pid']) && $_GET['pid'] != "" && is_numeric($_GET['pid']) && $_GET['pid'] >= 0)
-		$page = $db->real_escape_string($_GET['pid']);
-	else
-		$page = 0;
-	if(isset($_GET['query']))
-		$search = $db->real_escape_string($_GET['query']);
-	else
-		$search = '';
-	$user = new user();
-	if($search != "")
-	{
-		$tmp = explode(" ",$search);
-		$rsearch = '&amp;query='.$search;
-		$search = '';
-		foreach($tmp as $current)
-		{
-			$search .= "'\"$current\"' ";
-		}
-		$search .= 'IN BOOLEAN MODE';
-	}
-	else
-		$rsearch = '';
-	if($search != '')
-		$query = "SELECT COUNT(*) FROM $forum_topic_table as t1 JOIN $forum_post_table AS t2 ON (MATCH(t2.post) AGAINST($search)>0.5)";
-	else
-		$query = "SELECT COUNT(*) FROM $forum_topic_table";
-	$result = $db->query($query);
-	$row = $result->fetch_assoc();
-	$numrows = $row['COUNT(*)'];
-	if($search != "")
-		$query = "SELECT t1.id, t1.topic, t1.last_updated, t1.priority, t1.author FROM $forum_topic_table AS t1 JOIN $forum_post_table AS t2 ON (MATCH(t2.post) AGAINST($search)>0.5) ORDER BY t1.priority DESC, t1.last_updated DESC LIMIT $page, $limit";
-	else
-		$query = "SELECT id, topic, last_updated, author, locked, priority FROM $forum_topic_table  ORDER BY priority DESC, last_updated DESC LIMIT $page, $limit";
-	$result = $db->query($query) or die($db->error());
-	if($user->gotpermission('delete_forum_topics') || $user->gotpermission('pin_forum_topics'))
-		print '<th width="10%">Tools</th>';
-	echo '</tr>';
-	while($row = $result->fetch_assoc())
-	{
-		$que = "SELECT COUNT(*) FROM $forum_post_table WHERE topic_id='".$row['id']."'";
-		$res = $db->query($que) or die($db->error());
-		$ret = $res->fetch_assoc();
-		$replies = $ret['COUNT(*)']-1;
-		$date_now =	$misc->date_words($row['last_updated']);
-		$sticky = "";
-		$locked = "";
-		if($row['priority'] =="1")
-			$sticky ="Sticky: ";
-		if($row['locked']=="1")
-			$locked =' <span class="locked-topic">(locked)</span>';
-			
-		print '<tr>';
-		print '<td>'.$sticky.'<a href="?page=forum&amp;s=view&amp;id='.$row['id'].'">'.$row['topic'].'</a>'.$locked.'</td><td>'.$row['author'].'</td><td>'.$date_now.'</td><td>'.$replies.'</td>'; 
-		if($row['priority'] == 0)
-		{
-			if($user->gotpermission('pin_forum_topics'))
-				print '<td><a href="index.php?page=forum&amp;s=edit&amp;pin=1&amp;id='.$row['id'].'&amp;pid='.$page.'">Pin</a> | ';
-		}
-		else
-		{
-			if($user->gotpermission('pin_forum_topics')) 
-				print '<td><a href="index.php?page=forum&amp;s=edit&amp;pin=0&amp;id='.$row['id'].'&amp;pid='.$page.'">Unpin</a> | ';
-		}
-		if($user->gotpermission('delete_forum_topics'))
-			print ' <a href="index.php?page=forum&amp;s=remove&amp;fid='.$row['id'].'&amp;pid='.$page.'">Delete</a></td>'; 
-		echo '</tr>';
-	}
-	echo '</table><div class="paginator"><div id="paginator">';
-	$misc = new misc();
-	print $misc->pagination($_GET['page'],$_GET['s'],$row['id'],$limit,$page_limit,$numrows,$_GET['pid'],$_GET['tags'],$_GET['query']);
-	echo '</div><br /><div id="footer"><a href="#" onclick="showHide(\'new_topic\'); return false;">New Topic</a> | <a href="'.$site_url.'/help/">Help</a>';
-?>	
-</div></div><form method="post" action="index.php?page=forum&amp;s=add" id="new_topic" style="display:none">
-<table><tr><td>
-Topic:<br/>	
-<input type="text" name="topic" value=""/>
-</td></tr><tr><td>
-Post:<br />
-<textarea name="post" rows="4" cols="6" style="width: 600px; height: 200px;"></textarea>
-</td></tr><tr><td>
-<input type="hidden" name="conf" id='conf' value="0"/>
-</td></tr><tr><td>
-<input type="submit" name="submit" value="Create topic"/>
-</td></tr></table></form>
-<script type="text/javascript">
-//<![CDATA[
-document.getElementById('conf').value=1;
-//]]></script></body></html>
+	//Get last poster
+	$lastpostedid = $db->exec('SELECT author FROM '.$f3->get('forum_post_table').' WHERE topic_id = ? ORDER BY creation_date DESC LIMIT 1',array(1=>$thread['id']));
+
+    //Convert results for template
+    $threadresult[$resultcount]['id'] = $thread['id'];
+    $threadresult[$resultcount]['replies'] = ($repliescount[0]["count"])-1;
+    $threadresult[$resultcount]['last_updated_by'] = $lastpostedid[0]['author'];
+    $threadresult[$resultcount]['last_updated_by_name'] = $user->get_username($lastpostedid[0]['author']);
+    $threadresult[$resultcount]['author'] = $thread['author'];
+    $threadresult[$resultcount]['authorname'] = $user->get_username($thread['author']);    
+    $threadresult[$resultcount]['date_now'] = $misc->date_words($thread['last_updated']);
+    $threadresult[$resultcount]['priority'] = $thread['priority'];
+    $threadresult[$resultcount]['locked'] = $thread['locked'];
+    $threadresult[$resultcount]['topic'] = $thread['topic'];
+    $threadresult[$resultcount]['lastpage'] = ceil($repliescount[0]["count"] / 20);
+    $resultcount++;
+}
+
+//Set post thread for template
+$f3->set('threads',$threadresult);
+$f3->set('canpin',$canpin);
+$f3->set('candelete',$candelete);
+$f3->set('canlock',$canlock);
+?>
